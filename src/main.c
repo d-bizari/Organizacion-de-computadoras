392345ece561258cbde78d0db525cb978f168980
Daniela Carrero <danielacarreror@hotmail.com>
2020-05-16 12:45:50 -0300

    first commit assembly

--- src/main.c
+++ src/main.c
@@ -1,5 +1,8 @@
-#include <stdio.h>
+#include <unistd.h>
+extern size_t merge_sort(const char *);
 
-int main(int argc, const char * argv[]){
-	printf( "\nHello World\n\n" );
+int main(int argc, char * const argv[]){
+    char *msg = "Hola mundo.\n";
+    write(1, msg, merge_sort(msg));
+    return 0;
 }
