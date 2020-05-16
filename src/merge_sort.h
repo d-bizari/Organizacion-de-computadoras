53889fde93559021efc0f07ef43370277595361a
Daniela Carrero <danielacarreror@hotmail.com>
2020-05-16 10:58:39 -0300

    delete branch

--- src/merge_sort.h
+++ src/merge_sort.h
@@ -7,7 +7,6 @@
 #include <stdio.h>
 
 void merge_sort(int *vec, size_t len);
-int * clone_vector(int *vec, int from, size_t to);
 void merge(int *vec, const int *left, const int *right, int leftLen, int rightLen);
 
 #define ORGANIZACION_DE_COMPUTADORAS_MERGE_SORT_H
